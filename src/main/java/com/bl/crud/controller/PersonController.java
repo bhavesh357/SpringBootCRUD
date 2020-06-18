50d88295cf8b7b49bab3c93a305a730136a091b7
bhavesh357 <bkadam357@gmail.com>
2020-06-18 15:35:10 +0530

    ADDED: list method in controller

--- src/main/java/com/bl/crud/controller/PersonController.java
+++ src/main/java/com/bl/crud/controller/PersonController.java
@@ -1,14 +1,23 @@
 package com.bl.crud.controller;
 
 
+import com.bl.crud.model.Person;
 import com.bl.crud.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PersonController {
 
     @Autowired
     private PersonService service;
+
+    @GetMapping("/products")
+    public List<Person> list() {
+        return service.listAll();
+    }
 }
